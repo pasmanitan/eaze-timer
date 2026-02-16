579e268a3126442aff78e40d146af10113a7789a
pasmanitan <pasmanitan@gmail.com>
2026-02-16 17:19:59 -0500

    swsws

--- src/assets/serviceWorker.js
+++ src/assets/serviceWorker.js
@@ -33,3 +33,17 @@ self.addEventListener("fetch", (event) => {
     }),
   );
 });
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== cacheName)
+            .map((key) => caches.delete(key)),
+        ),
+      ),
+  );
+});
