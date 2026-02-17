995e4d303665b69c308e35d7b5e44bbcf9ac657d
pasmanitan <pasmanitan@gmail.com>
2026-02-17 08:45:39 -0500

    sw straight from mdn

--- src/assets/serviceWorker.js
+++ src/assets/serviceWorker.js
@@ -1,6 +1,7 @@
-const cacheName = "eaze-timer-v1";
+const cacheName = "eaze-timer-v20";
 const cacheAssets = [
   "/",
+  "/index.html",
   "/favicon.ico",
   "/index.js",
   "/index.css",
@@ -15,73 +16,40 @@ const cacheAssets = [
 ];
 
 self.addEventListener("install", (event) => {
-  console.log("Service worker is installed");
   event.waitUntil(
-    caches
-      .open(cacheName)
-      .then((cache) => {
-        console.log("Caching assets");
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting()),
+    (async () => {
+      const cache = await caches.open(cacheName);
+      cache.addAll(cacheAssets);
+    })(),
   );
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service worker is activated");
-
-  // removes old caches
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return cacheNames.map((cache) => {
-        if (cache !== cacheName) {
-          console.log("Clearing old caches");
-          caches.delete(cache);
-        }
-      });
-    }),
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== cacheName) {
+            return caches.delete(name);
+          }
+          return undefined;
+        }),
+      );
+      await clients.claim();
+    })(),
   );
 });
 
-const cacheFirst = async ({ request, fallbackUrl }) => {
-  // First try to get the resource from the cache.
-  const responseFromCache = await caches.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-
-  // If the response was not found in the cache,
-  // try to get the resource from the network.
-  try {
-    const responseFromNetwork = await fetch(request);
-    // If the network request succeeded, clone the response:
-    // - put one copy in the cache, for the next time
-    // - return the original to the app
-    // Cloning is needed because a response can only be consumed once.
-    putInCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
-  } catch (error) {
-    // If the network request failed,
-    // get the fallback response from the cache.
-    const fallbackResponse = await caches.match(fallbackUrl);
-    if (fallbackResponse) {
-      return fallbackResponse;
-    }
-    // When even the fallback response is not available,
-    // there is nothing we can do, but we must always
-    // return a Response object.
-    return new Response("Network error happened", {
-      status: 408,
-      headers: { "Content-Type": "text/plain" },
-    });
-  }
-};
-
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    cacheFirst({
-      request: event.request,
-      fallbackUrl: "/index.html",
-    }),
+    (async () => {
+      const cache = await caches.open(cacheName);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return new Response(null, { status: 404 });
+    })(),
   );
 });
