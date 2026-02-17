13c656a2fff32db50870de20bebb2fa582c685c7
pasmanitan <pasmanitan@gmail.com>
2026-02-17 09:44:34 -0500

    I give up

--- src/assets/serviceWorker.js
+++ src/assets/serviceWorker.js
@@ -7,18 +7,18 @@ const CACHE_NAME = `eaze-${VERSION}`;
 // The static resources that the app needs to function.
 const APP_STATIC_RESOURCES = [
   "/",
-  "index.html",
-  "index.js",
-  "index.css",
-  "favicon.ico",
-  "site.webmanifest",
-  "jingle.wav",
-  "android-chrome-192x192.png",
-  "android-chrome-512x512.png",
-  "apple-touch-icon.png",
-  "favicon-16x16.png",
-  "favicon-32x32.png",
-  "serviceWorker.js",
+  "./index.html",
+  "./index.js",
+  "./index.css",
+  "./favicon.ico",
+  "./site.webmanifest",
+  "./jingle.wav",
+  "./android-chrome-192x192.png",
+  "./android-chrome-512x512.png",
+  "./apple-touch-icon.png",
+  "./favicon-16x16.png",
+  "./favicon-32x32.png",
+  "./serviceWorker.js",
 ];
 
 self.addEventListener("install", (event) => {
